de3bb3a04be6bf58723a9563d577f1af58d2be90
Kyle Grewe <kyle.grewe@caci.com>
2021-10-12 00:01:44 -0400

    Added javascript

---
+++ js/roobie.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $('#content').load("./../components/button/button.html");
+    $('#content').load("./../components/container/container.html");
+    $('#content').load("./../components/grid/grid.html");
+    $('#content').load("./../components/input/input.html");
+    $('#content').load("./../components/navbar/navbar.html");
+
+});

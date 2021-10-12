2aaab6fb24b1700f69b1e478c4a4889bd8beee51
Kyle Grewe <kyle.grewe@caci.com>
2021-10-12 00:21:16 -0400

    Added sidemenu

--- js/roobie.js
+++ js/roobie.js
@@ -1,9 +1,3 @@
 $(function() {
 
-    $('#content').load("./../components/button/button.html");
-    $('#content').load("./../components/container/container.html");
-    $('#content').load("./../components/grid/grid.html");
-    $('#content').load("./../components/input/input.html");
-    $('#content').load("./../components/navbar/navbar.html");
-
 });

bde2e94e82f9d7bbb2e0e9215ad14b4c2b0ef3bc
Kyle Grewe <kyle.grewe@caci.com>
2021-10-21 22:50:29 -0400

    Updated links with hover state

--- roobie.js
+++ roobie.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+    index();
+});
+
+/* 
+Loads index and navbar
+*/
+function index() {
     load("nav.html", "nav");
     load("home.html", "main");
-});
+}
 
 /*
 Loads html from a file
@@ -9,4 +16,19 @@ Loads html from a file
 function load(filename, id) {
     console.log("Loading " + filename);
     $('#' + id).load(filename);
+}
+
+/*
+Adds the active class to a clicked link
+*/
+function active(elem) {
+    console.log("Setting active link");
+    // Get all links
+    var links = document.getElementsByTagName("a");
+    // Remove active classes
+    for (i = 0; i < links.length; i++) {
+        links[i].classList.remove('active')
+    }
+    // Add 'active' class to clicked element
+    elem.classList.add('active');
 }

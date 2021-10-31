d0950e245e058902a572b4ffb5b3a09a6cbfbd53
Kyle Grewe <kyle.grewe@caci.com>
2021-10-31 15:39:13 -0400

    Updated docs, added debug switch

--- roobie.js
+++ roobie.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+/*
+Prints debug messages when true.
+*/
+var debug = true;
+
+/* 
+Load basic content on browser refresh
+*/
+ready(function() {
+    if (debug) {
+        console.log("Debug ON");
+    } else {
+        console.log("Debug OFF");
+    }
     index();
 });
 
@@ -14,6 +27,9 @@ function index() {
 Executes a function when the document is ready
 */
 function ready(fn) {
+    if (debug) {
+        console.log("Executing function on document ready");
+    }
     if (document.readyState === "complete" || document.readyState === "interactive") {
         setTimeout(fn, 1);
     } else {
@@ -25,7 +41,9 @@ function ready(fn) {
 Load html to id.
 */
 function load(filename, id) {
-    console.log("Loading " + filename + " into element with id " + id);
+    if (debug) {
+        console.log("Loading " + filename + " into element with id " + id);
+    }
     var element = document.getElementById(id);
     $(element).empty();
     $(element).load(filename);
@@ -35,7 +53,9 @@ function load(filename, id) {
 Add class to an element.
 */
 function addClass(element, className) {
-    console.log("Adding class " + className + " to element " + element);
+    if (debug) {
+        console.log("Adding class " + className + " to element " + element);
+    }
     element.classList.add(className);
 }
 
@@ -43,7 +63,9 @@ function addClass(element, className) {
 Removes a class from elements with specified tag name.
 */
 function removeClassByTagName(tagName, className) {
-    console.log("Removing class " + className + " by tag name " + tagName);
+    if (debug) {
+        console.log("Removing class " + className + " by tag name " + tagName);
+    }
     var elements = document.getElementsByTagName(tagName);
 
     for (i = 0; i < elements.length; i++) {

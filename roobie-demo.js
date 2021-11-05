040599bf5ef56a44ca2be2f341680695e0001d80
Kyle Grewe <kyle.grewe@caci.com>
2021-11-05 16:24:07 -0400

    Refactored, updated docs layout

---
+++ roobie-demo.js
@@ -0,0 +1,9 @@
+ready(function() {
+    index();
+});
+
+function index() {
+    loadById("nav", "nav.html");
+    loadById("main", "home.html");
+    loadById("footer", "footer.html");
+}
--- roobie.js
+++ roobie.js
@@ -1,25 +1,24 @@
+/**
+ * Global variables.
+ */
 var appName = "Roobie";
 var debugMode = true;
 
-/* 
-Load index content on browser refresh
-*/
-ready(function() {
-    index();
-});
-
-/* 
-Loads all content
-*/
-function index() {
-    load("nav.html", "nav");
-    load("home.html", "main");
-    load("footer.html", "footer");
+function getId(id) {
+    return document.getElementById(id);
 }
 
-/* 
-Prints a debug message if debug mode is active
-*/
+function getClass(className) {
+    return document.getElementsByClassName(className);
+}
+
+function getTag(tagName) {
+    return document.getElementsByTagName(tagName);
+}
+/**
+ * Print debug message to console if Debug Mode is ON.
+ * @param {string} message Text to print alonside custom date and app details configured in roobie.js.
+ */
 function debug(message) {
     var ts = new Date();
     if (debugMode) {
@@ -27,9 +26,11 @@ function debug(message) {
     }
 }
 
-/* 
-Executes a function when the document is ready
-*/
+/**
+ * Execute function on DOMContentLoaded, aka document ready.
+ * 
+ * @param {*} fn Function to execute upon document ready.
+ */
 function ready(fn) {
     debug("Executing document ready");
     if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -39,42 +40,81 @@ function ready(fn) {
     }
 }
 
-/*
-Load html to id.
-*/
-function load(fileName, id) {
-    debug("Loading " + fileName + " into element with id " + id);
-    var element = document.getElementById(id);
-    $(element).empty();
-    $(element).load(fileName);
+/**
+ * Load file content into an element by id.
+ * 
+ * @param {string} id Id of HTML element.
+ * @param {string} file Path to file.
+ */
+function loadById(id, file) {
+    debug("Load [File] " + file + " -> [Id] " + id);
+    var element = getId(id);
+    $(element).load(file);
 }
 
-/*
-Add class to an element.
-*/
-function addClass(element, className) {
-    debug("Adding class " + className + " to element " + element);
+/**
+ * Add class to an element by id.
+ * 
+ * @param {string} id Id of HTML element
+ * @param {string} className CSS class
+ */
+function addClassById(id, className) {
+    debug("Add [Class] " + className + " -> [Id]" + id);
+    getId(id).classList.add(className);
+}
+
+/**
+ * Add class to an element.
+ * 
+ * @param {HTMLElement} element HTML element
+ * @param {string} className CSS class
+ */
+function addClassByElement(element, className) {
+    debug("Add [Class] " + className + " -> [Element]" + element);
     element.classList.add(className);
 }
 
-/*
-Removes a class from elements with specified tag name.
-*/
-function removeClassByTagName(tagName, className) {
-    debug("Removing class " + className + " by tag name " + tagName);
-    var elements = document.getElementsByTagName(tagName);
-
+/**
+ * Remove CSS class by tag.
+ * 
+ * @param {string} tagName HTML tag name
+ * @param {string} className CSS class
+ */
+function removeClassByTag(tagName, className) {
+    debug("Remove [Class] " + className + " -> [Tag] " + tagName);
+    var elements = getTag(tagName);
     for (i = 0; i < elements.length; i++) {
         elements[i].classList.remove(className);
     }
 }
 
-/*
-Toggles hide or show element with specified id.
+/**
+Removes a class from element by id.
 */
+function removeClassById(id, className) {
+    debug("Remove [Class] " + className + " -> [Id] " + id);
+    getId(id).classList.remove(className);
+}
+
+/**
+Removes a class from elements by class name.
+*/
+function removeClassByClass(className, removeClassName) {
+    debug("Remove [Class] " + removeClassName + " -> [Class] " + className);
+    var elements = getClass(className);
+    for (i = 0; i < elements.length; i++) {
+        elements[i].classList.remove(removeClassName);
+    }
+}
+
+/**
+ * Toggle the display of an element based on the current state.
+ * 
+ * @param {string} id Id of element.
+ */
 function toggleById(id) {
-    debug("Hiding element by id " + id);
-    var element = document.getElementById(id);
+    debug("Toggle [Id]" + id);
+    var element = getId(id);
     if (element.style.display === "none") {
         element.style.display = "flex";
     } else {
@@ -82,31 +122,14 @@ function toggleById(id) {
     }
 }
 
-/*
-Hides elements with specified id.
-*/
-function hideById(id) {
-    debug("Hiding element by id " + id);
-    var element = document.getElementById(id);
-    element.style.display = "none";
-}
-
-/*
-Show elements with specified id.
-*/
-function showById(id) {
-    debug("Showing element by id " + id);
-    var element = document.getElementById(id);
-    element.style.display = "flex";
-}
-
-/*
-Toggles hide or show elements with specified tag name.
-*/
-function toggleByTagName(tagName) {
-    debug("Toggle elements by tag name " + tagName);
-    var elements = document.getElementsByTagName(tagName);
-
+/**
+ * Toggle display of an element by tagName name.
+ * 
+ * @param {string} tagName tagName name of elements.
+ */
+function toggleByTag(tagName) {
+    debug("Toggle [Tag] " + tagName);
+    var elements = getTag(tagName);
     for (i = 0; i < elements.length; i++) {
         if (elements[i].style.display === "none") {
             elements[i].style.display = "flex";
@@ -116,37 +139,12 @@ function toggleByTagName(tagName) {
     }
 }
 
-
-/*
-Hides elements with specified tag name.
-*/
-function hideByTagName(tagName) {
-    debug("Hiding elements by tag name " + tagName);
-    var elements = document.getElementsByTagName(tagName);
-
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-    }
-}
-
-/*
-Show elements with specified tag name.
-*/
-function showByTagName(tagName) {
-    debug("Showing elements by tag name " + tagName);
-    var elements = document.getElementsByTagName(tagName);
-
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.display = "flex";
-    }
-}
-
 /** 
  * Rotate element by x amount of degrees.  If the previous rotation is positive,
  * it will reset the rotation to 0.
  */
-function rotateById(degrees, id) {
-    var element = document.getElementById(id);
+function rotateById(id, degrees) {
+    var element = getId(id);
     var transform = element.style.transform;
     debug('Previous rotation before cut ' + transform);
 

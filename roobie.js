960c3feb1d3968c09731bcdab107d2b234d1c2bb
Kyle Grewe <kyle.grewe@caci.com>
2021-11-07 22:18:27 -0500

    Added base theme

--- roobie.js
+++ roobie.js
@@ -186,6 +186,45 @@ function removeClassByTag(tagName, className) {
 }
 
 /**
+ * Add atrribute to an element by id.
+ * 
+ * @param {string} id Id to select
+ * @param {string} attribute Attribute to add
+ * @param {string} value Value of attribute
+ */
+function addAttributeById(id, attribute, value) {
+    debug("Add [Attribute] " + attribute + '=' + value + " -> [Id]" + id);
+    getId(id).setAttribute(attribute, value);
+}
+
+/**
+ * Add attribute to an element.
+ * 
+ * @param {HTMLElement} element HTML element to select
+ * @param {string} attribute Attribute to add
+ * @param {string} value Value of attribute
+ */
+function addAttributeByElement(element, attribute, value) {
+    debug("Add [Attribute] " + attribute + '=' + value + " -> [Element]" + element);
+    element.setAttribute(attribute, value);
+}
+
+/**
+ * Remove atrribute by tag name.
+ * 
+ * @param {string} tagName Tag name to select
+ * @param {string} attribute Attribute to remove
+ */
+function removeAttributeByTag(tagName, attribute) {
+    debug("Remove [Attribute] " + attribute + " -> [Tag] " + tagName);
+    var elements = getTag(tagName);
+    for (i = 0; i < elements.length; i++) {
+        elements[i].removeAttribute(attribute);
+    }
+}
+
+
+/**
  * Remove CSS class from an element by id.
  * 
  * @param {string} id Id to select

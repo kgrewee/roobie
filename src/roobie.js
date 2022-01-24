f73fc7d89d9ca520efad1c01136d33deb240602a
Kyle Grewe <kyle.grewe@caci.com>
2022-01-24 03:06:24 -0500

    Cleanup, updated animations

--- src/roobie.js
+++ src/roobie.js
@@ -73,16 +73,29 @@ function collapseMenu(event, className) {
 /**
  * Triggers an animation on an element.
  * 
- * @param {HTMLElement} element 
- * @param {string} className 
+ * @param {HTMLElement} element Element to animate
+ * @param {string} className Class name of animation
  */
-function animation(element, className) {
-    console.log("Animating " + element);
+function animationByElement(element, className) {
+    console.log("Animating by element " + element);
     element.classList.remove(className);
     void element.offsetWidth;
     element.classList.add(className);
 }
 
+/**
+ * Triggers an animation on an element by id.
+ * 
+ * @param {string} id Id of element to animate
+ * @param {string} className Class name of animation
+ */
+function animationById(id, className) {
+    console.log("Animating by id " + id);
+    let element = getId(id);
+    element.classList.remove(className);
+    void element.offsetWidth;
+    element.classList.add(className);
+}
 /**
  * Expand responsive menus.
  * 

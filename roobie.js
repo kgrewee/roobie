2c4c143cad6da305524f990f181b35a1fb3fcd02
Kyle Grewe <kyle.grewe@caci.com>
2022-01-30 17:09:40 -0500

    Add checkMenu back to js

--- roobie.js
+++ roobie.js
@@ -225,6 +225,7 @@ ready(() => {
     MEDIACOLLAPSEL.addEventListener("change", (event) => collapseMenu(event, 'rsp-menu-l'));
     MEDIAEXPANDXL.addEventListener("change", (event) => expandMenu(event, 'rsp-menu-xl'));
     MEDIACOLLAPSEXL.addEventListener("change", (event) => collapseMenu(event, 'rsp-menu-xl'));
+    checkMenu();
 })
 
 

84904de70e8ef795a5fbde6c4eefcbc64ceb2789
Kyle Grewe <kyle.grewe@caci.com>
2022-01-31 00:47:44 -0500

    Add accordion component

--- roobie.js
+++ roobie.js
@@ -388,7 +388,26 @@ function toggleByTag(tagName) {
             elements[i].style.display = "none";
         }
     }
-}var slideIndex = 1;
+}ready(() => {
+    var acc = document.getElementsByClassName("accordion-header");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+});var slideIndex = 1;
 ready(() => {
     showSlides(slideIndex);
 })

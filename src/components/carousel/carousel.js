e44818f0e153055f22c4114e897cd69774482ddb
Kyle Grewe <kyle.grewe@caci.com>
2022-02-01 02:07:28 -0500

    Updated carousel to use mostly js.  Cleaned up classes

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,31 +1,86 @@
-var slideIndex = 1;
 ready(() => {
-    showSlide(slideIndex);
-})
-
-function incrementSlide(n) {
-    showSlide(slideIndex += n);
-}
-
-function showSlide(n) {
-    var i;
-    var slides = document.getElementsByClassName("carousel-slide");
-    var dots = document.getElementsByClassName("carousel-dots");
-    if (n > slides.length) {
-        slideIndex = 1;
+    let carousels = document.getElementsByClassName("carousel");
+    for (let carousel of carousels) {
+        let index = carousel.getAttribute("index");
+        if (index == "" || index == null) {
+            index = 0;
+            carousel.setAttribute("index", "0");
+        }
+        showSlide(carousel.getAttribute("id"), index);
     }
-    if (n < 1) {
-        slideIndex = slides.length;
+
+    // Previous button click event
+    let previousButtons = document.getElementsByClassName("carousel-previous");
+    for (let btn of previousButtons) {
+        btn.addEventListener("click", function(e) {
+            showSlide(this.closest(".carousel").getAttribute("id"), +this.closest(".carousel").getAttribute("index") - 1);
+        });
     }
-    for (i = 0; i < slides.length; i++) {
+
+    // Next button click event
+    let nextButtons = document.getElementsByClassName("carousel-next");
+    for (let btn of nextButtons) {
+        btn.addEventListener("click", function(e) {
+            showSlide(this.closest(".carousel").getAttribute("id"), +this.closest(".carousel").getAttribute("index") + 1);
+        });
+    }
+
+    // Indicator click event
+    let indicatorContainers = document.getElementsByClassName("carousel-indicators");
+    for (let container of indicatorContainers) {
+        for (let indicator of container.children) {
+            indicator.addEventListener("click", function(e) {
+                showSlide(this.closest(".carousel").getAttribute("id"), +this.getAttribute("index"));
+            });
+        }
+    }
+});
+
+/**
+ * Show carousel slide by index.
+ * 
+ * @param {string} id Id of carousel element
+ * @param {number} index Index of slide to show
+ */
+function showSlide(id, index) {
+    debug("Show carousel [Id] " + id + " slides by index " + index);
+    let element = document.getElementById(id);
+
+    // Get carousel slides
+    let slides = element.querySelectorAll(".carousel-slide");
+
+    // Check if the index is out of bounds
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    if (slides[slideIndex - 1]) {
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
+
+    // Show slide index
+    if (slides[index]) {
+        slides[index].style.display = "block";
     }
 
+    let indicators = element.querySelectorAll(".carousel-indicators")[0];
+    if (indicators) {
+        indicators = indicators.children;
+        // Remove highlighted indicators
+        for (let i = 0; i < indicators.length; i++) {
+            indicators[i].classList.remove("active");
+        }
+
+        // Highlight active indicator
+        if (indicators[index]) {
+            indicators[index].classList.add("active");
+        }
+    }
+
+    // Update index attribute
+    element.setAttribute("index", index);
 }

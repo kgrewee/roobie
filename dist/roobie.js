8f720871adc95d5e9abd6cc7b98d3d35548fbda5
Kyle Grewe <kyle.grewe@caci.com>
2022-03-08 18:06:12 -0500

    Migrated to node-minify due to minify errors

--- dist/roobie.js
+++ dist/roobie.js
@@ -598,15 +598,14 @@ function loadById(id, file) {
         }
     };
     request.send();
-}const appName = "Roobie";
-const debugMode = true;
-
-/**
+}/**
  * Print debug message to console if Debug Mode is ON.
  * 
  * @param {string} message Text to print alonside custom date and app details
  */
 function debug(message) {
+    const appName = "Roobie";
+    const debugMode = true;
     if (debugMode) {
         console.log(appName + ' ' + new Date().toISOString() + '   ' + message);
     }

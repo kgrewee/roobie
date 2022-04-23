bcf24d418419ea81927eeedbfcba3edead1ab82f
Kyle Grewe <kyle.grewe@caci.com>
2022-04-23 18:25:20 -0400

    Add minified version for modularized files:

--- minify.js
+++ minify.js
@@ -31,3 +31,69 @@ minify({
     output: 'dist/roobie.bundle.css',
     callback: function(err, min) {}
 })
+
+// Animations
+minify({
+    compressor: uglifyES,
+    input: 'dist/roobie.animations.js',
+    output: 'dist/roobie.animations.js',
+    callback: function(err, min) {}
+})
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.animations.css',
+    output: 'dist/roobie.animations.css',
+    callback: function(err, min) {}
+})
+
+// Animations
+minify({
+    compressor: uglifyES,
+    input: 'dist/roobie.animations.js',
+    output: 'dist/roobie.animations.js',
+    callback: function(err, min) {}
+})
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.animations.css',
+    output: 'dist/roobie.animations.css',
+    callback: function(err, min) {}
+})
+
+// Components
+minify({
+    compressor: uglifyES,
+    input: 'dist/roobie.components.js',
+    output: 'dist/roobie.components.js',
+    callback: function(err, min) {}
+})
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.components.css',
+    output: 'dist/roobie.components.css',
+    callback: function(err, min) {}
+})
+
+// Layout
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.layout.css',
+    output: 'dist/roobie.layout.css',
+    callback: function(err, min) {}
+})
+
+// Themes
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.themes.css',
+    output: 'dist/roobie.themes.css',
+    callback: function(err, min) {}
+})
+
+// Utilties
+minify({
+    compressor: cleanCSS,
+    input: 'dist/roobie.utilities.css',
+    output: 'dist/roobie.utilities.css',
+    callback: function(err, min) {}
+})
